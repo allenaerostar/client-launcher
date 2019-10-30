0a5fdf351d1237c541e6d10d9cbe9feaf0fb75c5
allen <iceyallen569@hotmail.com>
2019-10-30 14:46:13 -0400

    Added .gitignore and removed __Pycache and .idea

--- DietStory-routing/DietStory-routing/wsgi.py
+++ DietStory-routing/DietStory-routing/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for DietStory-routing project.
+WSGI config for dietstory-routing project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DietStory-routing.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dietstory-routing.settings')
 
 application = get_wsgi_application()
