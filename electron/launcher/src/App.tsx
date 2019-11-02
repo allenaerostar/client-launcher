4365b287c7034a94bd6572349e27c2784ca011eb
Rayden <kevinxu.444@gmail.com>
2019-11-02 17:15:21 -0400

    attempt at making login and registration

--- electron/launcher/src/App.tsx
+++ electron/launcher/src/App.tsx
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
-import { isSafeToUnpackElectronOnRemoteBuildServer } from 'app-builder-lib/out/platformPackager';
+import { isSafeToUnpackElectronOnRemoteBuildServer } from 'app-builder-lib/out/platformPackager'; 
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Root from './components/Root/Root';
+import Registration from './components/Registration/Registration';
 
 import './App.css';
 
+const ipc = window.require('electron').ipcRenderer;
+
 class App extends Component {
 
-  state = {}
+  state = {
+    token: true
+  }
+
+  ipcTest = () => {
+    ipc.send('echo-message', 'Hello testing!');
+  }
+
+  // <Route exact path="/test" component={Test} />
 
   render() {
     return (
-      <div className="container">Hello</div>
+      <Router>
+          <div className="App">
+            <div className="container">
+              <Route exact path="/" component={Root} />
+              <Route path="/registration" component={Registration} />
+            </div>
+          </div>
+      </Router>
     );
   }
 }
