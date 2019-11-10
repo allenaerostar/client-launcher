a408ac1567090c8c4b9f522da0eecd90e8ab5c46
Rayden <kevinxu.444@gmail.com>
2019-11-10 17:55:11 -0500

    Finished registration function

--- electron/launcher/src/components/Registration/Registration.tsx
+++ electron/launcher/src/components/Registration/Registration.tsx
@@ -1,38 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { userActions } from '../../_actions';
 
 const Registration = props => {
 
+  const [username, setUsername] = useState('');
+  const [password1, setPassword1] = useState('');
+  const [password2, setPassword2] = useState('');
+  const [email, setEmail] = useState('');
+  const [birthday, setBirthday] = useState('');
+
   const handleSubmit = (e) => {
+    let user = {
+      username: username,
+      password1: password1,
+      password2: password2,
+      email: email,
+      birthday: birthday
+    }
     e.preventDefault();
-    props.register();
+    props.register(user);
   }
 
   const formFields = [
     {
       name: 'username',
+      label: 'Username',
       type: 'text'
     },
     {
       name: 'password',
+      label: 'Password',
+      type: 'password'
+    },
+    {
+      name: 'confirm-password',
+      label: 'Confirm Password',
       type: 'password'
     },
     {
       name: 'email',
+      label: 'Email',
       type: 'email'
+    },
+    {
+      name: 'birthday',
+      label: 'Birthday',
+      type: 'text'
     }
   ];
   // function for futurecases, handling fast input changes
-  // const handleChange = (e) => {
-  //   const name = e.target.name;
-  //   const value = e.target.value;
-  //   if (name === 'username') {
-  //     setUsername(value);
-  //   } else if (name === 'password') {
-  //     setPassword(value);
-  //   }
-  // }
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    if (name === 'username') {
+      setUsername(value);
+    } else if (name === 'password') {
+      setPassword1(value);
+    } else if (name === 'confirm-password') {
+      setPassword2(value);
+    } else if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'birthday') {
+      setBirthday(value);
+    }
+  }
 
   return (
     <>
@@ -41,37 +73,19 @@ const Registration = props => {
         {
           formFields.map( input => (
             <>
-              <label htmlFor={input.name}>{input.name}:</label>    
+              <label htmlFor={input.name}>{input.label}:</label>    
               <input
                 id={input.name}
                 type={input.type}
                 name={input.name}
-                placeholder={input.name}>
+                placeholder={input.label}
+                onChange={handleChange}  
+              >
               </input>
+              <br />
             </>
           ))
         }
-        {/* <label htmlFor="username">Username:</label>
-        <input
-          id="username"
-          type="text"
-          name="username"
-          placeholder="Username">  
-        </input>
-        <label htmlFor="password">Password:</label>
-        <input
-          id="password"
-          type="text"
-          name="password"
-          placeholder="Password">
-        </input>
-        <label htmlFor="email">Email:</label>
-        <input
-          id="email"
-          type="text"
-          name="email"
-          placeholder="email">
-        </input> */}
         <button type="submit">Register</button>
       </form>
     </>
