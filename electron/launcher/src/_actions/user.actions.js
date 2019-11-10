6f6aea416f28e185b5152d1340ecd0d4afa71d0f
Rayden <kevinxu.444@gmail.com>
2019-11-10 17:55:11 -0500

    Finished registration function

--- electron/launcher/src/_actions/user.actions.js
+++ electron/launcher/src/_actions/user.actions.js
@@ -1,34 +1,47 @@
 import history from '../_helpers/history';
 
+const ipc = window.require('electron').ipcRenderer;
+
 // Action creators for user handling
 // API calls should go here?
 const login = (username, password) => {
-  history.push('/');
-  return {
-    type: 'LOGIN'
-  };
-  // dispatch({
-  //   type: 'LOGIN',
-  //   payload: {
-  //     'username': username,
-  //     'password': password,
-  //   }
-  // })
-
-  
+   return {
+     type: 'LOGIN',
+     payload: {
+       username: username,
+       password: password
+     }
+   };
+   
 }
 const logout = () => {
   return {
     type: 'LOGOUT',
   }
 }
-const register = (user) => async (dispatch, getState) => {
-  dispatch({
-    type: 'REGISTER',
-    payload: user
-  });
+// const register = (user) => async (dispatch, getState) => {
+//   dispatch({
+//     type: 'REGISTER',
+//     payload: user
+//   });
 
-  history.push('/login');
+//   history.push('/login');
+// }
+
+const register = (user) => {
+  return (dispatch) => {
+    dispatch({type: 'REGISTER_START'});
+
+    ipc.send('http-registration', user);
+
+    ipc.on('http-registration-success', (e, res) => {
+      dispatch({type: 'REGISTER_SUCCESS'});
+    });
+
+    ipc.on('http-registration-fail', (e, err) => {
+      dispatch({type: 'REGISTER_FAILED'});
+    });
+  }
 }
 
 export const userActions = {
--- electron/launcher/src/index.tsx
+++ electron/launcher/src/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
 
 import App from './App';
 import reducers from './_reducers';
 
 ReactDOM.render(
   // Load up our redux store with our reducers
-  <Provider store={createStore(reducers)}>
+  <Provider store={createStore(reducers, applyMiddleware(thunk))}>
     <App />
   </Provider>,
   document.getElementById('root')
