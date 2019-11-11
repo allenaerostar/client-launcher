1545226366aaa1c07a0ef8a65ee116117e51808d
Situ William <situ_william@yahoo.com>
2019-11-11 18:03:07 -0500

    feature: completed async actions for login/registration using placeholder api calls for now

--- electron/launcher/src/index.tsx
+++ electron/launcher/src/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import App from './App';
 import reducers from './_reducers';
 
+// Hook up thunk middleware to redux store
+const store = createStore(reducers, applyMiddleware(thunk));
+
 ReactDOM.render(
   // Load up our redux store with our reducers
-  <Provider store={createStore(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
