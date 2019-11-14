7ebb81dc0f976be4cbe9a6ef014ff3eb064b713a
Situ William <situ_william@yahoo.com>
2019-11-14 01:06:27 -0500

    feat: add validateField function

---
+++ electron/launcher/src/components/FormErrors.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export const FormErrors = ({ formErrors }) => {
+  return (
+    <div className='formErrors'>
+      {
+        Object.keys(formErrors).map((fieldName, i) => {
+          if (formErrors[fieldName].length > 0) {
+            return (
+              <p key={i}>{fieldName} {formErrors[fieldName]}</p>
+            )
+          } else {
+            return '';
+          }
+        })
+      }
+    </div>
+  );
+}
+export default FormErrors;
