334510d9e3b52af4eacba7935a20542069d291e9
Allen Lee <Allenyuklun.lee@mail.utoronto.ca>
2019-11-15 12:20:38 -0500

    Swaggerized routes

--- dietstory-routing/registration/views.py
+++ dietstory-routing/registration/views.py
@@ -29,6 +29,82 @@ class SignupView(views.APIView):
         return JsonResponse({'message': "Welcome to signup page. Please enter username, email, password, and birthday."}, status=200)
 
     def post(self, request, *args, **kwargs):
+        """
+        summary: Post Signup description
+        description: Registers a user account using an username, email, password, and birthday.
+        parameters:
+            - name: email
+              schema:
+                  type: string
+                  format: email
+              description: >
+                  An email of the form user@domain.com
+              required: true
+            - name: username
+              schema:
+                  type: string
+              description: >
+                  Username consisting of only alphabet characters, numbers, underscores, and hyphens.
+              required: true
+            - name: password1
+              schema:
+                  type: string
+              description: >
+                  Password consisting of only alphabet characters, numbers, underscores, and hyphens.
+              required: true
+            - name: password2
+              schema:
+                  type: string
+              description: >
+                  Confirm password1 by retyping password1.
+              required: true
+            - name: birthday
+              schema:
+                  type: string
+                  format: date
+              description: >
+                  Birthday in the form of YYYY-MM-DD.
+              required: true
+        tags:
+            - SignupView
+        responses:
+            201:
+                content:
+                    application/json:
+                        schema:
+                            type: object
+                            properties:
+                                message:
+                                    type: string
+                                    description: Successful creation.
+            400:
+                content:
+                    application/json:
+                        schema:
+                            type: object
+                            properties:
+                                message:
+                                    type: string
+                                    description: That account already exists.
+            400:
+                content:
+                    application/json:
+                        schema:
+                            type: object
+                            properties:
+                                message:
+                                    type: string
+                                    description: Inputs have invalid format.
+            500:
+                content:
+                    application/json:
+                        schema:
+                            type: object
+                            properties:
+                                message:
+                                    type: string
+                                    description: Account creation was not successful.
+        """
 
         params = SignupForm(request.data)
 
@@ -66,6 +142,64 @@ class VerifyView(views.APIView):
     permission_classes = (permissions.AllowAny,)
 
     def post(self, request, *args, **kwargs):
+        """
+        summary: Post Verification description
+        description: Verifies a user account using an email, and verification code.
+        parameters:
+            - name: email
+              schema:
+                  type: string
+                  format: email
+              description: >
+                  An email of the form user@domain.com
+              required: true
+            - name: verify_token
+              schema:
+                  type: string
+              description: >
+                  A verification token sent by dietstory-devs email.
+              required: true
+        tags:
+            - VerifyView
+        responses:
+            200:
+                content:
+                    application/json:
+                        schema:
+                            type: object
+                            properties:
+                                message:
+                                    type: string
+                                    description: Your account has been verified.
+            400:
+                content:
+                    application/json:
+                        schema:
+                            type: object
+                            properties:
+                                message:
+                                    type: string
+                                    description: Inputs have invalid format.
+            400:
+                content:
+                    application/json:
+                        schema:
+                            type: object
+                            properties:
+                                message:
+                                    type: string
+                                    description: Verification code is invalid.
+            500:
+                content:
+                    application/json:
+                        schema:
+                            type: object
+                            properties:
+                                message:
+                                    type: string
+                                    description: Account verification was not successful.
+        """
+
         params = VerifyForm(request.data)
 
         if not params.is_valid():
@@ -96,6 +230,58 @@ class SendVerificationView(views.APIView):
     permission_classes = (permissions.AllowAny,)
 
     def post(self, request, *args, **kwargs):
+        """
+        summary: Post Send Verification description
+        description: Resend verification code to email associated with account.
+        parameters:
+            - name: email
+              schema:
+                  type: string
+                  format: email
+              description: >
+                  An email of the form user@domain.com
+              required: true
+
+        tags:
+            - SendVerificationView
+        responses:
+            200:
+                content:
+                    application/json:
+                        schema:
+                            type: object
+                            properties:
+                                message:
+                                    type: string
+                                    description: Verification code has been resent to the valid email address.
+            400:
+                content:
+                    application/json:
+                        schema:
+                            type: object
+                            properties:
+                                message:
+                                    type: string
+                                    description: No Email has been sent.
+            400:
+                content:
+                    application/json:
+                        schema:
+                            type: object
+                            properties:
+                                message:
+                                    type: string
+                                    description: Inputs have invalid format.
+            500:
+                content:
+                    application/json:
+                        schema:
+                            type: object
+                            properties:
+                                message:
+                                    type: string
+                                    description: Failed to send confirmation email.
+        """
 
         params = EmailForm(request.data)
 
@@ -124,6 +310,69 @@ class LoginView(views.APIView):
     permission_classes = (permissions.AllowAny,)
 
     def post(self, request, *args, **kwargs):
+        """
+        summary: Post Login description
+        description: Login account with username and password.
+        parameters:
+            - in: header
+              name: X-CSRFToken
+              schema:
+                  type: string
+              required: true
+            - name: username
+              schema:
+                  type: string
+              description: >
+                  Username of the account.
+              required: true
+            - name: password
+              schema:
+                  type: string
+              description: >
+                  Password of the account
+              required: true
+
+        tags:
+            - LoginView
+        responses:
+            200:
+                content:
+                    application/json:
+                        schema:
+                            type: object
+                            properties:
+                                name:
+                                    type: string
+                                    description: Username of the account.
+                                email:
+                                    type: string
+                                    description: Email of the account.
+                                is_active:
+                                    type: boolean
+                                    description: Verification status of the account.
+                                is_superuser:
+                                    type: boolean
+                                    description: If this user is admin, or not.
+            400:
+                content:
+                    application/json:
+                        schema:
+                            type: object
+                            properties:
+                                message:
+                                    type: string
+                                    description: Failed to login.
+            400:
+                content:
+                    application/json:
+                        schema:
+                            type: object
+                            properties:
+                                message:
+                                    type: string
+                                    description: Inputs have invalid format.
+
+        """
 
         params = LoginForm(request.data)
         if params.is_valid():
@@ -143,6 +392,28 @@ class LogoutView(views.APIView):
     permissions_classes = (permissions.AllowAny,)
 
     def post(self, request, *args, **kwargs):
+        """
+        summary: Post Logout description
+        description: Logout account that is logged in.
+        parameters:
+            - in: header
+              name: X-CSRFToken
+              schema:
+                  type: string
+              required: true
+        tags:
+            - LogoutView
+        responses:
+            200:
+                content:
+                    application/json:
+                        schema:
+                            type: object
+                            properties:
+                                message:
+                                    type: string
+                                    description: Successfully logged out.
+        """
         logout(request)
         return JsonResponse({'message': "Successfully logged out."}, status=200)
 
