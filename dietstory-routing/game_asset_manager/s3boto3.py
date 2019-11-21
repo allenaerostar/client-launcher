e6dd95b4a8578765e62c226d562d352ba01e22bf
Yilun Zhao <yl4zhao@uwaterloo.ca>
2019-11-21 17:50:14 -0500

    [FEAT] boto3 s3 mockable s3 client

--- dietstory-routing/game_asset_manager/s3boto3.py
+++ dietstory-routing/game_asset_manager/s3boto3.py
@@ -1,35 +1,45 @@
 import boto3
 from botocore.client import Config
 from django.conf import settings
+from botocore.stub import Stubber
 
 class S3Boto3Factory:
-	def get_s3_client(bucket=None):
-		if settings.AWS_CONFIG:
+	def get_s3_client(stubbed_response={}):
+		if hasattr(settings, 'AWS_CONFIG'):
 			config = settings.AWS_CONFIG
-			AWS_ACCESS_KEY_ID = config['AWS_ACCESS_KEY_ID']
-			AWS_SECRET_ACCESS_KEY = config['AWS_SECRET_ACCESS_KEY']
-			AWS_STORAGE_BUCKET_NAME = config['AWS_STORAGE_BUCKET_NAME']
-			AWS_DEFAULT_REGION = config['AWS_DEFAULT_REGION']
-			AWS_STORAGE_BUCKET_NAME = bucket if bucket else config['AWS_STORAGE_BUCKET_NAME']
 
+			# Create s3 client
 			s3_client = boto3.client(
 				's3',
-				aws_access_key_id=AWS_ACCESS_KEY_ID,
-				aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-				region_name=AWS_DEFAULT_REGION,
+				aws_access_key_id=config['AWS_ACCESS_KEY_ID'],
+				aws_secret_access_key=config['AWS_SECRET_ACCESS_KEY'],
+				region_name=config['AWS_DEFAULT_REGION'],
 				config=Config(signature_version='s3v4'))
 
-			return S3Client(s3_client=s3_client, bucket=AWS_STORAGE_BUCKET_NAME)
+			return S3Client(s3_client=s3_client, bucket=config['AWS_STORAGE_BUCKET_NAME'])
 
 		else:
-			return S3Client(s3_client=boto3.client('s3'), bucket=bucket, stubbed=True) # stub boto3 client method
+			# stub boto3 client method
+			return S3Client(s3_client=boto3.client('s3'), 
+				bucket=None, 
+				stubbed=True, 
+				stubbed_response=stubbed_response)
 
 class S3Client:
-	def __init__(self, s3_client, bucket, stubbed=False):
+	def __init__(self, s3_client, bucket, stubbed=False, stubbed_response={}):
+		self.stubbed = stubbed
 		self.__s3_client = s3_client
 		self.__bucket = bucket
-		self.__stubbed = stubbed
 
+		if stubbed:
+			self.__stubber = Stubber(self.__s3_client)
+			# Wrap or override methods in stubs
+			response = stubbed_response['generate_presigned_url'] if 'generate_presigned_url' in stubbed_response else None
+			self.get_object_presigned_url = self.stub_get_object_presigned_url(self.get_object_presigned_url, response)
+			# Activate stubber
+			self.__stubber.activate()
+
+	# Get object URL
 	def get_object_presigned_url(self, key, expires=300):
 		try:
 			res = self.__s3_client.generate_presigned_url(
@@ -38,10 +48,16 @@ class S3Client:
 					'Bucket': self.__bucket,
 					'Key': key },
 				ExpiresIn=expires)
-		except e:
-			print(e)
+		except Exception as e:
+			print(e) # TODO: Logging
 			res = None
 
 		return res;
 
+	# Get object URL stub decorator
+	def stub_get_object_presigned_url(self, function, response):
+		# Override function
+		def override(*args, **kwargs):
+			return response
+		return override
 
