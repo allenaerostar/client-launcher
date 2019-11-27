de707dac873d01c9175edba9c3e52ca82c4a8a4f
Yilun Zhao <yl4zhao@uwaterloo.ca>
2019-11-27 18:12:47 -0500

    [UPDATE]: Added features to /game-files/download

---
+++ dietstory-routing/game_asset_manager/validate_forms.py
@@ -0,0 +1,28 @@
+import re
+from django import forms
+from django.core.validators import validate_email, validate_slug, BaseValidator
+
+VERSION_FORMAT = "^[vV]?([0-9]*)[.]([0-9]*)$"
+
+class VersionIdValidator(BaseValidator):
+	def check_version_format(version):
+		return re.search(VERSION_FORMAT, version) != None
+
+	compare = lambda self, a, b: not VersionIdValidator.check_version_format(a)
+	message = "Version must follow the form: ({})".format(VERSION_FORMAT)
+
+
+class RequestGameAssetForm(forms.Form):
+	filename = forms.CharField(max_length=128)
+	versionid = forms.CharField(max_length=16, required=False, validators=[VersionIdValidator(True)])
+
+	def get_version_values(self):
+		if self.cleaned_data.get('versionid'):
+			search = re.search(VERSION_FORMAT, self.cleaned_data.get('versionid'))
+			if search:
+				major = search.group(1)
+				minor = search.group(2)
+				return major, minor
+
+		return None, None
+
