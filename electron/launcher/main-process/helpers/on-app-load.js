bc8b5a3d3ccfadbd06ac7c4cb6335cd6b59da8ba
Rayden <kevinxu.444@gmail.com>
2019-12-01 10:26:48 -0500

    Windows: Adds game path to Windows Defender exclusion list

--- electron/launcher/main-process/helpers/on-app-load.js
+++ electron/launcher/main-process/helpers/on-app-load.js
@@ -1,5 +1,42 @@
 const fs = require('fs').promises;
 const path = require('path');
+const exec = require('child_process').exec;
+
+// CHECKS WINDOWS DEFENDER'S EXCLUSION PATH
+async function checkWinDefender(){
+  return new Promise((resolve, reject) => {
+    let command = '$pref = Get-MpPreference; $pref.ExclusionPath';
+    let options = {
+      shell: 'powershell.exe', 
+      windowsHide: true
+    }
+
+    exec(command, options, (error, stdout, stderr) => {
+      if(error){
+        reject(error);
+      }
+      resolve(stdout.includes(gameInstallationPath));
+    });
+  });
+}
+
+// ADDS GAME FOLDER INTO WINDOWS DEFENDER EXCLUSION PATH
+async function addExclusionPath(){
+  return new Promise((resolve, reject) => {
+    let command = `Start-Process powershell -Verb RunAs -WindowStyle Hidden -ArgumentList 'Add-MpPreference -ExclusionPath "${gameInstallationPath}"'`;
+    let options = {
+      shell: 'powershell.exe', 
+      windowsHide: true
+    }
+
+    exec(command, options, (error, stdout, stderr) => {
+      if(error){
+        reject(error);
+      }
+      resolve(0);
+    });
+  });
+}
 
 async function load(){
   // MAKE `Dietstory` DIRECTORY IF IT DOES NOT EXIST
@@ -17,6 +54,18 @@ async function load(){
   } catch(error){
     if(error.code === 'ENOENT'){
       await fs.mkdir(error.path);
+    }
+  }
+
+  // ADDS GAME INSTALLTION FOLDER TO WINDOWS DEFENDER EXLUSION LIST IF NOT ALREADY THERE
+  if(process.platform === 'win32'){
+    try{
+      let excluded = await checkWinDefender();
+      if(!excluded){
+        await addExclusionPath();
+      }
+    } catch(error){
+      console.log(error);
     }
   }
 }
