a70f5a4af3859076cc07e40819c525ac09d92dde
Kevin <kevinxu.444@gmail.com>
2019-12-14 22:50:08 -0500

    added request wrapper to inject csrf and session token into header

--- electron/launcher/main-process/ipc-listeners/user-logout.js
+++ electron/launcher/main-process/ipc-listeners/user-logout.js
@@ -1,4 +1,4 @@
-const request = require('request-promise');
+const request = require('helpers/request-wrapper').request;
 const ipc = require('electron').ipcMain;
 
 const config = require('config.json').DJANGO_SERVER;
