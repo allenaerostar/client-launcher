0a10a9c37a5a6aa7184c8aa844672a3935b5e514
Kevin <kevinxu.444@gmail.com>
2019-12-17 19:17:49 -0500

    removed redundant encrypt/decrypt functions

--- electron/launcher/main-process/helpers/login.js
+++ electron/launcher/main-process/helpers/login.js
@@ -120,11 +120,7 @@ const autoLogin = () => {
       return keytar.getPassword('Dietstory', userInfo.name).then(password => {
         return aes256.decrypt(password).then(decryptedPassword => {
           return {...userInfo, password: decryptedPassword};
-<<<<<<< HEAD
         });
-=======
-        })
->>>>>>> created new helper file for encrypt/decrypt. Added encryption to password before storing in Credential manager.
       })
       .catch(error => {
         throw error;
