dd336d7e9002bb9c1443989ec946333dea411fc1
Kevin <kevinxu.444@gmail.com>
2019-12-20 01:30:19 -0500

    project now builds properly on windows for windows

--- electron/launcher/main-process/electron.js
+++ electron/launcher/main-process/electron.js
@@ -1,6 +1,7 @@
 require('app-module-path').addPath(__dirname); // <-- MUST BE FIRST LINE TO ENABLE ABSOLUTE PATH
 const electron = require("electron");
 const app = electron.app;
+const { autoUpdater } = require('electron-updater');
 const BrowserWindow = electron.BrowserWindow;
 const isDev = require("electron-is-dev");
 const path = require("path");
@@ -23,16 +24,17 @@ function createWindow() {
             nodeIntegration: true
         }
     });
-    mainWindow.loadURL(isDev? "http://localhost:3000": `file://${path.join(__dirname, "../build/index.html")}`);
+    mainWindow.loadURL(isDev? "http://localhost:3000": `file://${path.join(__dirname, "../index.html")}`);
     mainWindow.on("closed", () => (mainWindow = null));
     mainWindow.once('ready-to-show', () => {mainWindow.show()});
 }
 
 app.on("ready", () => {
     createWindow();
-    setTimeout(() => {
-        mainWindow.webContents.send('launcher-update-ready');
-    }, 5000);
+    // CHECKS FOR UPDATE EVERY 2 HOURS
+    setInterval(() => {
+        autoUpdater.checkForUpdatesAndNotify();
+    }, 72000000);
 });
 app.on("window-all-closed", () => {if (process.platform !== "darwin") {app.quit();}});
 app.on("activate", () => {if (mainWindow === null) {createWindow();}});
@@ -43,6 +45,12 @@ require('ipc-listeners/user-logout');
 require('ipc-listeners/user-registration');
 require('ipc-listeners/file-manager');
 
-ipc.on('launcher-update-install', e => {
-    console.log('kappa');
+// ONCE UPDATE IS DOWNLOADED, NOTIFY USER
+autoUpdater.on('update-downloaded', () => {
+    mainWindow.webContents.send('launcher-update-ready');
 })
+
+// USER CLICKED THE BUTTON TO INSTALL UPDATE IMMEDIATELY, INSTALL AND RESTART
+ipc.on('launcher-update-install', e => {
+    autoUpdater.quitAndInstall();
+});
