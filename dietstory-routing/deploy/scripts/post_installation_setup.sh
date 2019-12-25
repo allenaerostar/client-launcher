117628af02f212fafa77a04d44beccca2f96d817
Yilun Zhao <yl4zhao@uwaterloo.ca>
2019-12-25 17:19:12 -0500

    Fix - updating downloading configs to correct folder

--- dietstory-routing/deploy/scripts/post_installation_setup.sh
+++ dietstory-routing/deploy/scripts/post_installation_setup.sh
@@ -4,7 +4,7 @@ SERVER_PATH=/apps/dietstory-django
 
 # Download configurations from S3
 if [ "$APPLICATION_NAME" == "dietstory-django-app-dev" ]; then
-	aws s3 cp s3://dietstory-api-server-assets/dev/config/dietstory_secret_config.json ${SERVER_PATH}/dietstory_secret_config.json
+	aws s3 cp s3://dietstory-api-server-assets/dev/config/dietstory_secret_config.json ${SERVER_PATH}/dietstory-routing/dietstory_secret_config.json
 elif [ "$APPLICATION_NAME" == "dietstory-django-app-prod" ]; then
-	aws s3 cp s3://dietstory-api-server-assets/prod/config/dietstory_secret_config.json ${SERVER_PATH}/dietstory_secret_config.json
+	aws s3 cp s3://dietstory-api-server-assets/prod/config/dietstory_secret_config.json ${SERVER_PATH}/dietstory-routing/dietstory_secret_config.json
 fi 
