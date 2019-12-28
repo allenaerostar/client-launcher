2d85e00bc9ab697d5bec5fb5100fbd3bcb4b75a4
Benjamin Zhao <yl4zhao@uwaterloo.ca>
2019-12-28 00:42:00 -0500

    Fixing deployment (#47)

--- dietstory-routing/deploy/scripts/pre_installation_cleanup.sh
+++ dietstory-routing/deploy/scripts/pre_installation_cleanup.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-SERVER_NAME=dietstory-django-server
+SERVER_PATH=/apps/dietstory-django
 
-if [ "$(docker ps -q -f name=${SERVER_NAME})" ]; then
-    if [ "$(docker ps -aq -f status=running -f name=${SERVER_NAME})" ]; then
-        # stop existing server
-        docker stop ${SERVER_NAME}
-    fi
-
-    # Remove container
-    docker rm ${SERVER_NAME}
-fi
+sudo rm -rf ${SERVER_PATH}
