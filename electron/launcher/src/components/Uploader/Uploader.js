35ac2ac49b7f683169604afa6c4b93ac2252ff4d
Situ William <situ_william@yahoo.com>
2020-01-06 18:12:21 -0500

    fix: connect proper prop values

---
+++ electron/launcher/src/components/Uploader/Uploader.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import Dropzone from 'react-dropzone';
+
+import FileInputs from 'components/Uploader/FileInputs';
+
+import upload_icon from 'assets/icons/upload_icon.svg';
+
+// {
+//   version: 'v1.1',
+//     root: 'C:\Nexon',
+//       files: [
+//         'C:\Nexon\Map.wz',
+//         'C:\Nexon\Character.wz'
+//       ]
+// }
+
+// const { remote } = window.require('electron')
+
+const Uploader = ({uploadFiles, changeFileInfo, files}) => {
+  return (
+    <>
+      <h1>Upload Files</h1>
+      <div className="row">
+        <div className="col-12">
+          <Dropzone onDrop={acceptedFiles => uploadFiles(acceptedFiles)} >
+            {({ getRootProps, getInputProps }) => (
+              <div {...getRootProps()} className="dropzone">
+                <input {...getInputProps()} />
+                <img src={upload_icon} alt="cloud with arrow" />
+                <h2>Drag and drop or click here to upload files</h2>
+              </div>
+            )}
+          </Dropzone>
+        </div>
+      </div>
+      {
+        files ?
+          <div className="row">
+            <div className="col-12">
+              {
+                files.map((fileData, index) => (
+                  <FileInputs
+                    handleChange={changeFileInfo}
+                    fileData={fileData}
+                    index={index}
+                    key={index}
+                  />
+                ))
+              }
+            </div>
+          </div>
+          : null
+      }
+    </>
+  );
+}
+
+export default Uploader;
