93a7af3e94ab5e12db9d0c23adfd7908f341571a
Situ William <situ_william@yahoo.com>
2020-01-06 22:59:44 -0500

    feat: remove some unused imports

--- electron/launcher/src/components/Form/FormOverview.js
+++ electron/launcher/src/components/Form/FormOverview.js
@@ -1,29 +1,30 @@
 import React from 'react';
 
-const FormOverview = ({ inputs }) => {
-
-  const displayFileMetaData = (input) => {
-    input.files.map((file) => (
+const FormOverview = ({ inputs, files }) => {
+  console.log(inputs);
+  const displayFileMetaData = () => {
+    files.map((fileData) => (
       <>
-        <h4>File: {file.name}</h4>
-        <h4>Path: {file.path}</h4>
+        <h4>File: {fileData.file.name}</h4>
+        <h4>Path: {fileData.path}</h4>
       </>
     ));
   }
 
   return (
-    <div>
-      {
+    <div className="row">
+      <div className="col-12">
+      {inputs ?
         inputs.map((input) => (
-          'files' in input ?
-            displayFileMetaData(input)
-          :
             <>
               <h3>{input.name}</h3>
               <p>{input.value}</p>
             </>
         ))
+        : null
       }
+        <button type="submit" className="btn btn-success btn-block">Submit</button>
+      </div>
     </div>
   );
 }
