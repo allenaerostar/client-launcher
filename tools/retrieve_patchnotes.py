df2d8cda52025a10bf572f5621dd8be6baa7c8ce
tzwang1 <truman.wang1234@gmail.com>
2020-01-18 14:22:41 -0800

    Creating script to scrape patchnotes from github pull requests.

--- tools/retrieve_patchnotes.py
+++ tools/retrieve_patchnotes.py
@@ -1,6 +1,6 @@
 """
-This tool calls the Github API retrieve a list of pull requests and iterates through them
-looking for the PATCHNOTES: keyword.
+This tool calls the Github API retrieve a list of pull requests and iterates through
+them looking for the "PATCHNOTES:" keyword.
 """
 import argparse
 import logging
@@ -15,36 +15,13 @@ logger = logging.getLogger(__name__)
 parser = argparse.ArgumentParser(description='Retrieves patchnotes from Github pull requests.')
 parser.add_argument('--state', default='closed',
                     help='State that pull requsts should be in eg. (open, closed etc).')
-parser.add_argument('--startdate', default='',
-                    help='Date that pull requests should be greater than or equal to.')
-parser.add_argument('--enddate', default='',
-                    help='Date that pull requests should be less than or equal to.')
 parser.add_argument('--keyword', default='PATCHNOTES:',
                     help='Keyword to parse pull requests for.')
 
 args = parser.parse_args()
 
-if len(args.startdate) != 0:
-    try:
-        START_DATE = datetime.strptime(args.startdate, "%Y-%m-%d")
-    except TypeError:
-        logger.error("Invalid date: {}. Please enter date in the format 'YYYY-MM-DD', "
-                     "using 1 week before as the start date instead.".
-                     format(args.startdate))
-        START_DATE = datetime.today() - timedelta(days=7)
-else:
-    START_DATE = datetime.today() - timedelta(days=7)
-
-if len(args.enddate) != 0:
-    try:
-        END_DATE =  datetime.strptime(args.enddate, "%Y-%m-%d")
-    except TypeError:
-        logger.error("Invalid date: {}. Please enter date in the format 'YYYY-MM-DD', "
-                     "using todays date as the end date instaed.".
-                     format(args.enddate))
-        END_DATE = datetime.today()
-else:
-    END_DATE = datetime.today()
+START_DATE = datetime.today() - timedelta(days=7)
+END_DATE = datetime.today()
         
 KEYWORD = args.keyword
 
@@ -59,15 +36,16 @@ def retrieve_patchnotes():
     data = r.json()
 
     for pull_request in data:
-        print("_________________________")
         # We take the first 10 characters so the date format is in %YYYY-%MM-%DD.
         closed_date = datetime.strptime(pull_request["closed_at"][:10], "%Y-%m-%d")
         idx = pull_request["body"].find("PATCHNOTES:")
         idx = 1
         if (idx != -1 and START_DATE <= closed_date and closed_date <= END_DATE):
+            print("_________________________")
             print(pull_request["body"][idx:])
-            
-        print("__________________________")
 
 if __name__ == "__main__":
+    print("Retrieving patchnotes between {} and {}".format(START_DATE, END_DATE))
+    print("Looking for keyword: {}".format(KEYWORD))
+
     retrieve_patchnotes()
