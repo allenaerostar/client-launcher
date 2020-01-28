3a183edd1493c4558940482b09a1355c829b05fd
tzwang1 <truman.wang1234@gmail.com>
2020-01-27 16:09:50 -0800

    Using proper import for request.

--- electron/launcher/main-process/ipc-listeners/user-self-help.js
+++ electron/launcher/main-process/ipc-listeners/user-self-help.js
@@ -1,4 +1,4 @@
-const request = require('request-promise');
+const request = require('helpers/request-wrapper').request;
 const ipc = require('electron').ipcMain;
 
 const deleteCache = require('helpers/update-game-files').deleteCache;
