95c55faa61ef1cbaf2af0cb890ffc35115201482
Allen Lee <Allenyuklun.lee@mail.utoronto.ca>
2020-02-13 22:16:54 -0500

    Added api call to dietstory game server to retrive rewards

--- dietstory-routing/login_bonus/updater.py
+++ dietstory-routing/login_bonus/updater.py
@@ -6,6 +6,7 @@ import requests
 
 
 def update_login_bonus(account_id):
+    url = "http://dietstory-game-server:8485/duey"
     try:
         login_bonus_for_account = LoginBonus.objects.get(account_id=account_id)
     except LoginBonus.DoesNotExist:
@@ -30,15 +31,13 @@ def update_login_bonus(account_id):
                     login_bonus_reward_item = get_login_reward_item(login_bonus_for_account.reward_num, current_month, current_year)
                     if login_bonus_reward_item:
                         data = {'account_id': account_id, 'item_id': login_bonus_reward_item.item_id, 'quantity':login_bonus_reward_item.quantity,'mesos':0, 'sender':"Dietstory", 'time_limit':login_bonus_reward_item.time_to_expire}
-                        url = "172.18.0.2:8485/hello"
-                        #response = requests.get(url)
-                        #response = requests.post("compassionate_haslett:8485/duey", data=data)
-                        #if response.status_code == requests.codes.ok:
-                        #    login_bonus_for_account.reward_num += 1
-                        #    login_bonus_for_account.latest_reward_time = current_date
-                        #    login_bonus_for_account.save()
-                        #else:
-                            #response.raise_for_status()
+                        response = requests.post(url, data=data)
+                        if response.status_code == requests.codes.ok:
+                            login_bonus_for_account.reward_num += 1
+                            login_bonus_for_account.latest_reward_time = current_date
+                            login_bonus_for_account.save()
+                        else:
+                            response.raise_for_status("Failed to reach dietstory game server API")
             except IOError as e:
                 print("Sending login bonus failed")
                 raise e
@@ -51,14 +50,12 @@ def update_login_bonus(account_id):
                     data = {'account_id': account_id, 'item_id': login_bonus_reward_item.item_id,
                                 'quantity': login_bonus_reward_item.quantity, 'mesos': 0, 'sender': "Dietstory",
                                 'time_limit': login_bonus_reward_item.time_to_expire}
-                    url = "172.18.0.2:8485/hello"
-                    #response = requests.get(url)
-                    #response = requests.post("compassionate_haslett:8485/duey", data=data)
-                    #if response.status_code == requests.codes.ok:
-                    #    login_bonus_for_account = LoginBonus(account_id=account_id, latest_reward_time=current_date, reward_month=current_month)
-                    #    login_bonus_for_account.save()
-                    #else:
-                    #    response.raise_for_status()
+                    response = requests.post(url, data=data)
+                    if response.status_code == requests.codes.ok:
+                        login_bonus_for_account = LoginBonus(account_id=account_id, latest_reward_time=current_date, reward_month=current_month)
+                        login_bonus_for_account.save()
+                    else:
+                        response.raise_for_status("Failed to reach dietstory game server API")
 
         except IOError as e:
             print("Failed to create login bonus entry associated with the account id")
