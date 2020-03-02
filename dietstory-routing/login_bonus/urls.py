7b5c124e36c94af9d2a751a59997347a2b2c826f
Yilun Zhao <yl4zhao@uwaterloo.ca>
2020-03-01 23:38:50 -0500

    [CHORE] - Adding player previously obtained login reward route

--- dietstory-routing/login_bonus/urls.py
+++ dietstory-routing/login_bonus/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('rewards', views.RewardsView.as_view()),
     path('rewards/<int:reward_number>', views.RewardView.as_view()),
+    path('rewards/myreward', views.PlayerRewardView.as_view()),
     path('upload', views.UploadLoginBonusRewardsView.as_view()),
     path('rewards/collect', views.CollectReward.as_view())
 ]
