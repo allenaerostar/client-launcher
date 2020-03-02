aa0b07094309cd710de25f84b25bcc3749c0a379
Kevin <kevinxu.444@gmail.com>
2020-03-02 01:52:00 -0500

    incomplete

---
+++ electron/launcher/src/components/LoginRewards/LoginRewardsContainer.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+
+const LoginRewardsContainer = props => {
+  return(
+    <div style={containerStyle}></div>
+  );
+}
+
+const containerStyle = {
+  backgroundColor: 'black',
+  height: '300px',
+  width: '275px'
+}
+
+export default LoginRewardsContainer;
