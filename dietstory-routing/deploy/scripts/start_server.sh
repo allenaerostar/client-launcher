2cbcd5c5a194fa2187fba4b1a5a9497783e4a534
Benjamin Zhao <yl4zhao@uwaterloo.ca>
2020-03-03 23:47:52 -0500

    [CHORE] - Adding Django server to dietstory network (#92)

--- dietstory-routing/deploy/scripts/start_server.sh
+++ dietstory-routing/deploy/scripts/start_server.sh
@@ -2,12 +2,14 @@
 
 SERVER_NAME=dietstory-django-server
 SERVER_PATH=/apps/dietstory-django
+SERVER_NETWORK=dietstory-service
 
 source ~/.bashrc
 
 # Run Server
-sudo  docker run -d \
+sudo docker run -d \
 	-v ${SERVER_PATH}:/mnt \
 	-p 8000:8000 \
+	--net ${SERVER_NETWORK} \
 	--name=${SERVER_NAME} \
 	benjixd/dietstory-django-server
